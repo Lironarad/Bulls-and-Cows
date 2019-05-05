7ab531908ba579a3adcac80797a6698036879e1f
unknown <lironarad1@gmail.com>
2019-05-05 12:56:45 +0300

    add files to new repository

---
+++ SmartGuesser.hpp
@@ -0,0 +1,24 @@
+#pragma once
+#include "Guesser.hpp"
+#include "calculate.hpp"
+#include <unordered_set>
+#include <math.h>
+#include <iostream>
+
+
+/**
+ * ConstantGuesser is a guesser that always guesses the same string.
+ */
+class SmartGuesser: public bullpgia::Guesser {
+	private:
+		std::string Guess;
+		std::unordered_set<std::string> _set;
+		void full_search();
+
+	public:
+		std::string guess() override;
+		void startNewGame(uint Length) override;
+		void learn(std::string response) override;
+};
+
+//std::string numToGuess(int num, uint length);
---
+++ calculate.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <string>
+
+namespace bullpgia {
+
+	const std::string calculateBullAndPgia(const std::string chosen, const std::string guess);
+	void check(const std::string,const std::string);
+}
